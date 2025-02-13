38a13cc033b346f29cc3c867094d647052e7b29e
a001985 <magnus.wenzer@smhi.se>
2025-02-13 10:42:24 +0100

    New paths

--- src/nodc_occurrence_id/utils.py
+++ src/nodc_occurrence_id/utils.py
@@ -4,18 +4,15 @@ import pathlib
 
 logger = logging.getLogger(__name__)
 
+CONFIG_ENV = 'NODC_CONFIG'
 
 CONFIG_SUBDIRECTORY = 'nodc_occurrence_id'
 CONFIG_FILE_NAMES = []
 
 
 DATABASE_DIRECTORY = None
-if os.getenv('NODC_CONFIG'):
-    DATABASE_DIRECTORY = pathlib.Path(os.getenv('NODC_CONFIG')) / CONFIG_SUBDIRECTORY / 'DATABASES'
-# TEMP_DATABASE_DIRECTORY = pathlib.Path.home() / 'temp_nodc_config' / CONFIG_SUBDIRECTORY / 'DATABASES'
-
-
-DATABASES_URL = r'https://raw.githubusercontent.com/nodc-sweden/nodc_config/refs/heads/main/' + f'{CONFIG_SUBDIRECTORY}/DATABASES/'
+if os.getenv(CONFIG_ENV) and pathlib.Path(os.getenv(CONFIG_ENV)).exists():
+    DATABASE_DIRECTORY = pathlib.Path(os.getenv(CONFIG_ENV)) / CONFIG_SUBDIRECTORY
 
 
 def get_all_class_children_list(cls):
